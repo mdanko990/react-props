9c96ae1e00eb338e722a78532780947c9980181c
mdanko990 <62939304+mdanko990@users.noreply.github.com>
2021-10-02 14:32:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import First from './First';
 
 export default function App() {
   return (
     <div>
       Some data:
+      <ul>
+        {list.map(el => (
+            <First id={el.id} text = {el.text}/>
+          ))}
+      </ul>
     </div>
   );
 }
